b31b97c752416daf3eccc8429e36f7929fe81efd
Crazzys <elianaperezramos871@gmail.com>
2026-05-27 22:59:33 -0500

    Se trabaja en el contactenos

---
+++ src/main/java/com/neup/web/dto/ContactoDTO.java
@@ -0,0 +1,28 @@
+package com.neup.web.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ContactoDTO {
+
+    @NotBlank(message = "El nombre es obligatorio")
+    private String nombre;
+
+    @NotBlank(message = "El correo es obligatorio")
+    @Email(message = "Formato de correo inválido")
+    private String email;
+
+    @NotBlank(message = "El asunto es obligatorio")
+    private String asunto;
+
+    @NotBlank(message = "El mensaje es obligatorio")
+    private String mensaje;
+}
